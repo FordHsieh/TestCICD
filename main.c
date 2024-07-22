eacf255228a5307e028ba8adafee4d028a91b72a
阿村 <FordHsieh@users.noreply.github.com>
2024-07-22 16:07:04 +0800

    Update main.c

--- main.c
+++ main.c
@@ -18,6 +18,22 @@ int main()
    printf("This is test\n");
    printf("This is test\n");
    printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
 
    return 0;
 }
