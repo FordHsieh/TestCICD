ab0c44c634ef3dd92cd21df42011ab216b6a80e2
FordHsieh <fordhsieh@tw.atpinc.com>
2024-07-22 15:37:10 +0800

    update main.c

--- main.c
+++ main.c
@@ -4,6 +4,12 @@ int main()
 {
    printf("This is test\n");
    printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
+   printf("This is test\n");
 
    return 0;
 }
